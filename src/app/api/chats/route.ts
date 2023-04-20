db23fb449e36720e907929400bef0eed28273c4e
Antonio Sabino <aslimajr@outlook.com>
2023-04-20 12:57:04 +0000

    fix: corrigir messages

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/app/prisma/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
+  const body = await request.json()
   const chatCreated = await prisma.chat.create({
     data: {}
   })
