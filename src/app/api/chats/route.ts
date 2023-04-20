7a17c536503b0aa55d9d1b3324154548894f2944
Antonio Sabino <aslimajr@outlook.com>
2023-04-20 00:42:19 +0000

    feat: adicionar rota para listar chats

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,6 +1,20 @@
+import { prisma } from '@/app/prisma/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  const body = await request.json()
-  return NextResponse.json({ body })
+  const chatCreated = await prisma.chat.create({
+    data: {}
+  })
+
+  return NextResponse.json(chatCreated)
+}
+
+export async function GET(_request: NextRequest) {
+  const chats = await prisma.chat.findMany({
+    orderBy: {
+      created_at: 'desc'
+    }
+  })
+
+  return NextResponse.json(chats)
 }
