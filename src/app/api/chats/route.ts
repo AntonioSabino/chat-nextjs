7ffa355f121a927e6919b8a40f499753ae409745
Antonio Sabino <aslimajr@outlook.com>
2023-04-22 23:52:52 +0000

    feat: adicionar user_id a rota chats

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -2,10 +2,11 @@ import { prisma } from '@/app/prisma/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 import { withAuth } from '../helpers'
 
-export const POST = withAuth(async (request: NextRequest) => {
+export const POST = withAuth(async (request: NextRequest, token) => {
   const body = await request.json()
   const chatCreated = await prisma.chat.create({
     data: {
+      user_id: token.sub!,
       messages: {
         create: {
           content: body.message
@@ -21,8 +22,11 @@ export const POST = withAuth(async (request: NextRequest) => {
   return NextResponse.json(chatCreated)
 })
 
-export const GET = withAuth(async (_request: NextRequest) => {
+export const GET = withAuth(async (_request: NextRequest, token) => {
   const chats = await prisma.chat.findMany({
+    where: {
+      user_id: token.sub
+    },
     select: {
       id: true,
       messages: {
