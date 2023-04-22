c06e500d583363c05ce138664bddea831ab7ae2b
Antonio Sabino <aslimajr@outlook.com>
2023-04-22 23:40:42 +0000

    feat: adicionando user_id ao Model Chat

--- prisma/migrations/20230422234013_/migration.sql
+++ prisma/migrations/20230422234013_/migration.sql
@@ -1,14 +1,12 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `Chat` (
+    `id` VARCHAR(191) NOT NULL,
+    `remote_chat_id` VARCHAR(191) NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-  - You are about to drop the `Messages` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE `Messages` DROP FOREIGN KEY `Messages_chat_id_fkey`;
-
--- DropTable
-DROP TABLE `Messages`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Message` (
